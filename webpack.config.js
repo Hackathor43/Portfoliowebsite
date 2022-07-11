fc5e0412ea58d2efccba571099524dc17784ab81
Hemanth kumar <ganeshhemanth9686@gmail.com>
2022-07-11 12:29:09 +0530

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    hot: true
+ },
+}
+
